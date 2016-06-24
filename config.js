a1e23e61ae8b5b11d01aee17092e486176f2f364
duncan <duncangitonga292@gmail.com>
2016-06-25 00:06:10 +0300

    Hide key

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 var config = {
-  firebase: 'mymaps-f8053',
+  
   mapbox: {
-      accessToken: 'pk.eyJ1IjoiZHVuY2FuLW1hcHMiLCJhIjoiY2lwdTVrZnNjMDA0NWkxa3N1cXlkeHdibCJ9.kj76S5gHdib8p282FuicGA',
-      mapId: 'mapbox.satellite',
+      
   }
 }
